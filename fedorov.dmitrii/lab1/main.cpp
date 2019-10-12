a5566162cd0901ef19b9ea018a160c8bb6697816
bi <bi@bi-VirtualBox.(none)>
2019-10-12 20:22:12 +0300

    Add daemon

---
+++ fedorov.dmitrii/lab1/main.cpp
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <vector>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <syslog.h>
+
+#include <dirent.h>
+#include <ftw.h>
+
+#include <sys/stat.h>
+
+
+using namespace std;
+
+
+const string PID_FILENAME = "pid_info.txt"; 
+const string PROC_DIR     = "/proc";
+
+int          UPDATE_FREQUENCY;
+
+string       CONFIG_FILE;
+string       WORK_DIR;
+
+string       SRC_DIR;
+string       DST_DIR;
+
+bool         NEED_WORK = true;
+int          WAIT_OF_KILLING_TIME = 1;
+
+
+void CreateDirectory (const string& absPath, const string& name)
+{
+   string currentPath(get_current_dir_name()); 
+   chdir(absPath.c_str());
+
+   int mkdirErr = mkdir(name.c_str(), S_IRWXU);
+   if (mkdirErr) {
+      syslog(LOG_USER, "Something goes wrong while creating directory");
+      exit(EXIT_FAILURE);
+   }     
+
+   chdir(currentPath.c_str());
+}
+
+
+int UnlinkCB (const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) 
+{
+   int rv;
+
+   if (ftwbuf->level == 0) {
+      return 0;
+   }
+
+   rv = remove(fpath);
+   if (rv) {
+      perror(fpath);
+      syslog(LOG_USER, "Something goes wrong while deleating files");
+      exit(EXIT_FAILURE);
+   }  
+
+   return rv;
+}
+
+
+void ClearDirectory (const string& absPath) 
+{
+   nftw(absPath.c_str(), UnlinkCB, 64, FTW_DEPTH | FTW_PHYS); 
+}
+
+
+vector<string> GetContentList (const string& absPath)
+{
+   DIR*           dir;
+   struct dirent* ent;
+   vector<string> res;
+
+   if ((dir = opendir(absPath.c_str())) != NULL) {
+      while ((ent = readdir(dir)) != NULL) {
+         if (ent->d_type == DT_DIR) {
+            continue;
+         }
+         cout << ent->d_name << endl;
+         res.push_back(ent->d_name);
+      }
+      closedir(dir);
+   } else {
+      return vector<string>();
+   }
+   return res;
+}
+
+
+void CopyFile (const string& absPathSrc, const string& absPathDst)
+{
+   ifstream src(absPathSrc.c_str(), ios::binary);
+
+   if (!src.good()) {
+      syslog(LOG_USER, "Copy file goes bad");
+      return;
+   }   
+
+   ofstream dst(absPathDst.c_str(), ios::binary);
+   dst << src.rdbuf();
+}
+
+
+bool IsPngFormat (const string& filename)
+{
+   int last = filename.size() - 1;
+   return (filename.size() > 4 && filename[last] == 'g' && filename[last - 1] == 'n' && filename[last - 2] == 'p' && filename[last - 3] == '.');
+}
+
+
+void DoDaemonWork (void) 
+{
+   ClearDirectory(DST_DIR);
+   CreateDirectory(DST_DIR, "IMG");
+   CreateDirectory(DST_DIR, "OTHERS");
+   
+   vector<string> srcContent = GetContentList(SRC_DIR);
+   for (unsigned i = 0; i < srcContent.size(); i++) {
+      string filename = srcContent[i];
+      string srcFilePath = SRC_DIR + "/" + filename;
+      string dstFilePath;
+      
+      if (IsPngFormat(filename)) {
+         dstFilePath = DST_DIR + "/IMG/" + filename;
+      } else {
+         dstFilePath = DST_DIR + "/OTHERS/" + filename;
+      }
+
+      CopyFile(srcFilePath, dstFilePath);
+   }
+}
+
+
+bool LoadConfig ()
+{
+   string line;
+
+   fstream in(CONFIG_FILE.c_str());
+   if (in.is_open()) {
+      getline(in, line);
+      SRC_DIR = line;
+
+      getline(in, line);
+      DST_DIR = line;
+
+      getline(in, line);
+      UPDATE_FREQUENCY = atoi(line.c_str());
+
+      return true;
+   } 
+   return false;
+}
+
+
+void InitPidFile (void) 
+{
+   string pidFilePath = WORK_DIR + "/" + PID_FILENAME;
+   ofstream f(pidFilePath.c_str());
+   syslog(LOG_USER, "pid file created: %s, pid is %i", pidFilePath.c_str(), getpid());  
+   
+   f << getpid() << endl; 
+}
+
+
+string ReadPidFile (void) 
+{
+   string pidFilePath = WORK_DIR + "/" + PID_FILENAME;
+   fstream f(pidFilePath.c_str());
+   if (f.good()) {
+      string line;
+      getline(f, line);
+      return line;
+   }   
+
+   return string(""); 
+}
+
+
+void KillIfOpened (void)
+{
+   DIR*   dir;
+   string pid = ReadPidFile();
+   string pidFolder = (PROC_DIR + "/" + pid);
+   
+   if (pid == "") {
+      return;
+   }
+
+   syslog(LOG_USER, "Deamon already works - kill him");
+   if ((dir = opendir(pidFolder.c_str())) != NULL) {
+      kill(atoi(pid.c_str()), SIGTERM);  
+   }
+
+   while ((dir = opendir(pidFolder.c_str())) != NULL) {
+      sleep(WAIT_OF_KILLING_TIME);
+   }
+}
+
+
+void WorkProc (void) 
+{
+   while (true) {
+      if (NEED_WORK) {
+	 DoDaemonWork();
+      }
+      sleep(UPDATE_FREQUENCY);
+   }
+}
+
+
+void OnSignalRecieve(int sig)
+{
+   switch (sig) {
+      case SIGHUP:
+      { 
+         NEED_WORK = false;
+         syslog(LOG_USER, "Reload daemon's config by signal");
+         LoadConfig();
+         NEED_WORK = true;
+	 break;
+      }
+      case SIGTERM:
+      {
+         NEED_WORK = false;
+ 	 string pidFilePath = WORK_DIR + "/" + PID_FILENAME;
+         syslog(LOG_USER, "Terminate daemon by signal");
+	 unlink(pidFilePath.c_str());
+         closelog();
+	 exit(SIGTERM);
+	 break;
+      }
+   }
+}
+
+
+void InitDaemon (void)
+{    
+   openlog("daemon_lab", 0, LOG_USER);
+ 
+   umask(0);
+   setsid();
+   chdir("/"); 
+
+   KillIfOpened();
+   InitPidFile();
+		
+   close(STDIN_FILENO);
+   close(STDOUT_FILENO);
+   close(STDERR_FILENO);
+  
+   signal(SIGHUP, OnSignalRecieve);   
+   signal(SIGTERM, OnSignalRecieve);     
+}
+
+
+int main (int argc, char** argv) {
+   if (argc != 2) {
+      cout << "Bad argument to launch daemon: needed config file name" << endl;
+      return -1;
+   }     
+
+   string configFileName(argv[1]); 
+   WORK_DIR = string(get_current_dir_name());
+   CONFIG_FILE = WORK_DIR + "/" + configFileName;
+   
+   if (!LoadConfig()) {
+      cout << "Bad config file" << endl;
+      return -1;
+   }
+
+   int pid = fork();
+   if (pid == -1) {
+      cout << "Error: start daemon failed" << endl;
+      return -1;
+   } else if (!pid) {
+      InitDaemon();
+      WorkProc();
+   } else {
+      return 0;
+   }
+}
