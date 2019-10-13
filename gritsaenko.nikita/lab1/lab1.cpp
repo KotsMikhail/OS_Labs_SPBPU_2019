7f8c28153cfdead8b18fd72fb7fdaada925f02c2
Nikita Gritsaenko <nikita-gricaenko@ya.ru>
2019-10-13 22:44:42 +0300

    Fix remarks of lab1

--- gritsaenko.nikita/lab1/lab1.cpp
+++ gritsaenko.nikita/lab1/lab1.cpp
@@ -7,7 +7,11 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <syslog.h>
+#include <pwd.h>
+#include <ftw.h>
+#include <string.h>
 #include <sstream>
+#include <glob.h>
 
 std::string config_path;
 std::string pid_file_path = "/var/run/lab1_daemon";
@@ -25,30 +29,99 @@ namespace patch
     }
 }
 
+bool is_dir_exist(std::string& path) {
+    if (path[0] == '~') {
+         passwd *pw;
+         uid_t uid;
+
+        uid = geteuid();
+        pw = getpwuid(uid);
+        if (pw)
+            path.replace(0, 1, std::string("/home/") + pw->pw_name); 
+        else
+            syslog(LOG_WARNING, "WARNING: Couldn't find username by UID %u. There is no guarantee to find folder which path contains `~`.", uid);
+        
+    }
+    struct stat buffer;
+    if (path.length() != 0 && stat(path.c_str(), &buffer) == 0 && (S_ISDIR(buffer.st_mode)))
+        return true;
+    return false;
+}
+
+bool is_file_exist(const std::string &name) {
+    struct stat buffer;
+    if (name.length() != 0 && stat(name.c_str(), &buffer) == 0)
+        return true;
+    return false;
+}
+
+void copy_file(std::string src_path, std::string dst_path) {
+    //printf("copy file %s to %s \n", src_path.c_str(), dst_path.c_str());
+    std::ifstream src(src_path.c_str(), std::ios::binary);
+    std::ofstream dst(dst_path.c_str(), std::ios::binary);
+    dst << src.rdbuf();
+}
+
+static int rmFiles(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
+{
+    if (remove(pathname) < 0)
+    {
+        perror("ERROR: remove");
+        return -1;
+    }
+    return 0;
+}
+
 void clear_folder(const std::string& path) {
-    system((std::string("rm -rf ") + path).c_str());
-    system((std::string("mkdir ") + path).c_str());
+    nftw(path.c_str(), rmFiles, 10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS);
+    
+    if (mkdir(path.c_str(), 0777) == -1) {
+       printf("Unable to create directory %s \n", path.c_str()); 
+    }
 }
 
-void copy_bk_files(const std::string& src, const std::string& dst) {
-    system((std::string("find ") + src + std::string(" -name \\*.bk  -exec cp {} ") + std::string(dst) + std::string(" \\;")).c_str());
+void copy_bk_files(std::string& src, std::string& dst) {
+    if (!is_dir_exist(src)) {
+        printf("Source directory %s is not exist. Copy failed. \n", src.c_str());
+        return;
+    }
+    glob_t glob_result;
+    glob((src + std::string("/*")).c_str(), GLOB_TILDE, NULL, &glob_result);
+
+    for(unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
+        std::string path = glob_result.gl_pathv[i];
+        if (is_dir_exist(path))
+            copy_bk_files(path, dst);
+        
+        std::string::size_type idx;
+
+        idx = path.rfind('.');
+
+        if (idx != std::string::npos) {
+            std::string extension = path.substr(idx + 1);
+            if (extension == std::string("bk")) {
+                copy_file(src + std::string("/") + std::string(basename(path.c_str())), dst + std::string("/") + std::string(basename(path.c_str())));
+            }
+        }
+    }
 }
 
-
-bool is_dir(const char* path) {
-    struct stat buf;
-    stat(path, &buf);
-    return S_ISDIR(buf.st_mode);
-}
-
-void read_config_file() {
+int read_config_file() {
+    if (!is_file_exist(config_path.c_str())) {
+        printf("Config file %s is not exist. \n", config_path.c_str());
+        return EXIT_FAILURE;
+    }
     std::ifstream config_file(config_path.c_str());
     if (config_file.is_open() && !config_file.eof()) {
-        syslog (LOG_NOTICE, "Reading config file.");
         config_file >> folder1 >> folder2 >> interval;
         config_file.close();
         config_path = realpath(config_path.c_str(), NULL);
     }
+    if (folder1.length() == 0 || folder2.length() == 0 || interval == 0) {
+        printf("Empty args, please check config file %s. \n", config_path.c_str());
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
 void signal_handler(int signum) 
@@ -66,20 +139,7 @@ void signal_handler(int signum)
 
 static void create_daemon()
 {
-    std::ifstream pid_file(pid_file_path.c_str());
-
-    if (pid_file.is_open() && !pid_file.eof()) {
-        syslog (LOG_NOTICE, "Read pid file.");
-        pid_t prev_daemon_pid;
-        pid_file >> prev_daemon_pid;
-        std::string proc = "/proc/" + patch::to_string(prev_daemon_pid);
-        if (is_dir(proc.c_str())) {
-            syslog (LOG_NOTICE, "Kill previous daemon.");
-            kill(prev_daemon_pid, SIGTERM);
-        }
-    }
-
-    pid_file.close();
+    /* create new daemon */
 
     pid_t pid;
 
@@ -97,6 +157,14 @@ static void create_daemon()
     signal(SIGHUP, signal_handler);
     signal(SIGTERM, signal_handler);
 
+    pid = fork();
+
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
     umask(0);
 
     if (chdir("/") < 0) {
@@ -104,10 +172,27 @@ static void create_daemon()
         exit(EXIT_FAILURE);
     }
 
+    syslog (LOG_NOTICE, "New daemon.");
+
+    /* kill previous daemon */
+
+    std::ifstream pid_file(pid_file_path.c_str());
+
+    if (pid_file.is_open() && !pid_file.eof()) {
+        pid_t prev_daemon_pid;
+        pid_file >> prev_daemon_pid;
+        std::string proc = "/proc/" + patch::to_string(prev_daemon_pid);
+        syslog (LOG_NOTICE, "Kill previous daemon.");
+        kill(prev_daemon_pid, SIGTERM);
+    }
+
+    pid_file.close();
+
+    /* set new pid */
+
     std::ofstream pid_file_out(pid_file_path.c_str());
 
     if (pid_file_out.is_open()) {
-        syslog (LOG_NOTICE, "Set pid file.");
         pid_file_out << getpid();
         pid_file_out.close();
     }
@@ -126,17 +211,22 @@ int main(int argc, char **argv)
 
     config_path = argv[1];
 
-    read_config_file();
+    int last_error = read_config_file();
+    if (last_error != EXIT_SUCCESS)
+        return last_error;
 
+    fflush(stdout);
+    
     create_daemon();
 
-    while (1)
+    while (true)
     {
-        syslog (LOG_NOTICE, "Daemon does his work...");
+        if (!is_dir_exist(folder2))
+            printf("WARNING: Destination directory %s is not exist. It will be created. \n", folder2.c_str());
+
         clear_folder(folder2);
         copy_bk_files(folder1, folder2);
         sleep (interval);
-        syslog (LOG_NOTICE, "Daemon finished his work. Daemon is sleeping...");
     }
 
     syslog (LOG_NOTICE, "Close syslog");
