ff98fdf92663626b3e402cfe055862cd10b67a72
MChepulis <Michael.Chepulis@yandex.ru>
2019-10-22 22:44:59 +0300

    add-lab_1
    add build script
    add lab_1

---
+++ chepulis.mikhail/lab_1/lab_1.cpp
@@ -0,0 +1,307 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <ftw.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
+#include <dirent.h>
+#include <signal.h>
+
+#define PID_FILENAME "pid_info.txt"
+#define PROC_DIR "/proc"
+#define WAIT_OF_KILLING_TIME 1
+
+std::string work_dir;
+std::string config_path;
+std::string src_dir;
+std::string dst_dir;
+std::string pid_file_path;
+int update_freq;
+bool is_need_work;
+
+
+int UnlinkCB (const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) 
+{
+  int rv;
+
+  if (ftwbuf->level == 0) {
+    return 0;
+  }
+  rv = remove(fpath);
+  if (rv) {
+    perror(fpath);
+    syslog(LOG_USER, "Something goes wrong while deleating files \"%s\"", fpath);
+    //std::cout << "Something goes wrong while deleating files \"" << fpath <<"\"\n";
+    exit(EXIT_FAILURE);
+  }  
+  return rv;
+}
+
+
+void CreateDirectory (const std::string& absPath, const std::string& name)
+{
+   std::string currentPath(get_current_dir_name()); 
+   int error;
+   error = chdir(absPath.c_str());
+   if (error) {
+      syslog(LOG_USER, "Could not change directory \"%s\"", absPath.c_str());
+      //std::cout << "Could not change directory \"" << absPath << "\"" << std::endl;
+      exit(EXIT_FAILURE);
+   }  
+   error = mkdir(name.c_str(), S_IRWXU);
+   if (error) {
+      syslog(LOG_USER, "Could not create directory \"%s\"", absPath.c_str());
+      //std::cout << "Could not create directory \"" << absPath << "\"" << std::endl;
+      exit(EXIT_FAILURE);
+   }     
+   chdir(currentPath.c_str());
+}
+
+
+void CreateDaemon(const std::string configFileName){
+  work_dir = std::string(get_current_dir_name());
+  config_path = work_dir + "/" + configFileName;
+  pid_file_path = work_dir +"/" + PID_FILENAME;
+  is_need_work = true;
+  //std::cout << "WORK_DIR\t" << work_dir << "\n";
+  //std::cout << "CONFIG_FILE\t" << config_path << "\n";
+  //std::cout << "PID_FILE\t" << pid_file_path << "\n";
+}
+
+
+void LoadConfigFile(){
+  std::ifstream cfg_file(config_path.c_str());
+  if (!cfg_file.is_open() || cfg_file.eof()) {
+    syslog(LOG_ERR, "Could not open config file or it is empty");
+    exit(EXIT_FAILURE);
+  }
+  cfg_file >> src_dir >> dst_dir >> update_freq;
+  cfg_file.close();
+  //std::cout << "src_dir\t" << src_dir << "\n";
+  //std::cout << "dst_dir\t" << dst_dir << "\n";
+  //std::cout << "update_freq\t" << update_freq << "\n";
+}
+
+
+void signalHandler(int signal)
+{
+  switch (signal) {
+    case SIGHUP:
+    { 
+      is_need_work = false;
+      syslog(LOG_USER, "Reload daemon's config by signal");
+      //std::cout << "Reload daemon's config by signal\n";
+      LoadConfigFile();
+      is_need_work = true;
+      break;
+    }
+    case SIGTERM:
+    {
+      is_need_work = false;
+      syslog(LOG_USER, "Terminate daemon by signal");
+      //std::cout << "Terminate daemon by signal \n";
+      unlink(pid_file_path.c_str());
+      closelog();
+      exit(SIGTERM);
+      break;
+    } 
+  }
+}
+
+
+void InitPidFile () 
+{
+   std::ofstream f(pid_file_path.c_str());
+   syslog(LOG_USER, "pid file created: %s, pid is %i", pid_file_path.c_str(), getpid());  
+   //std::cout << "pid file created: " << pid_file_path.c_str() << ", pid is " << getpid() << std::endl;
+   f << getpid() << std::endl; 
+}
+
+
+std::string ReadPidFile () 
+{
+   std::fstream f(pid_file_path.c_str());
+   if (f.good()) {
+      std::string line;
+      getline(f, line);
+      return line;
+   }   
+   return std::string(""); 
+}
+
+
+void KillPrevDaemon() {
+  DIR*   dir;
+  std::string pid = ReadPidFile();
+  std::string pidFolder = (std::string(PROC_DIR) + "/" + pid);
+  //std::cout << "pidFolder\t" << pidFolder;
+  if (pid == "") {
+    return;
+  }
+
+  syslog(LOG_USER, "Deamon already works - kill him");
+  //std::cout << "Deamon already works - kill him\n";
+  if ((dir = opendir(pidFolder.c_str())) != NULL) {
+    kill(atoi(pid.c_str()), SIGTERM);  
+  }
+  while ((dir = opendir(pidFolder.c_str())) != NULL) {
+    sleep(WAIT_OF_KILLING_TIME);
+  }
+}
+
+void Init(){
+  int error;
+  //std::cout << "Init\n";
+  openlog("daemon_lab", 0, LOG_USER);
+  umask(0);
+  error = setsid();
+
+  if(error == -1)
+  {
+    syslog(LOG_USER, "Could not creat a new session");
+    exit(EXIT_FAILURE);
+  }
+
+  int pid = fork();
+  if (pid == -1) {
+    //std::cout << "ECould not fork" << std::endl;
+    syslog(LOG_USER, "Could not fork()");
+    exit(EXIT_FAILURE);
+  } else if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  error = chdir("/");
+  if (error < 0){
+    syslog(LOG_USER, "Could not change working directory to /");
+    exit(EXIT_FAILURE);
+  }
+
+  KillPrevDaemon();
+  InitPidFile();
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  signal(SIGHUP, signalHandler);   
+  signal(SIGTERM, signalHandler);
+}
+
+
+void ClearDirectory (const std::string& absPath) 
+{
+  nftw(absPath.c_str(), UnlinkCB, 64, FTW_DEPTH | FTW_PHYS); 
+}
+
+
+void CopyFile (const std::string& absPathSrc, const std::string& absPathDst)
+{
+  std::ifstream src(absPathSrc.c_str(), std::ios::binary);
+  if (!src.good()) {
+    //std::cout << "Copy file goes bad: src = " << absPathSrc << "\tdst = " << absPathDst << std::endl;
+    syslog(LOG_USER, "Copy file goes bad src = %s\tdst = %s", absPathSrc.c_str(), absPathDst.c_str());
+    return;
+  }   
+  std::ofstream dst(absPathDst.c_str(), std::ios::binary);
+  dst << src.rdbuf();
+}
+
+
+bool CompareExtantion(const std::string fileName, const std::string extantion){
+  if(fileName.size() <= extantion.size()){
+    return false;
+  }
+  int startInd = fileName.size() - extantion.size();
+  for(unsigned int i = 0; i < extantion.size(); i++){
+    if(fileName[startInd + i] != extantion[i]){
+      return false;
+    }
+  }
+  return true;   
+} 
+
+
+std::vector<std::string> GetContentList (const std::string& absPath)
+{
+  DIR*           dir;
+  struct dirent* ent;
+  std::vector<std::string> res;
+  if ((dir = opendir(absPath.c_str())) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      if (ent->d_type == DT_DIR) {
+        continue;
+      }
+      //std::cout << ent->d_name << std::endl;
+      res.push_back(ent->d_name);
+    }
+    closedir(dir);
+  } else {
+  return std::vector<std::string>();
+  }
+  return res;
+}
+
+
+void DoWork(){
+  //std::cout << "DoWork\n";
+  ClearDirectory(dst_dir);
+  //std::cout << "dst_dir\t" << dst_dir << "\n";
+  CreateDirectory(dst_dir, "IMG");
+  CreateDirectory(dst_dir, "OTHERS");
+  std::vector<std::string> srcContent = GetContentList(src_dir);
+  std::string filename;
+  std::string srcPath;
+  std::string dstPath;
+  for(unsigned int i = 0; i < srcContent.size(); i++){
+    filename = srcContent[i];
+    srcPath = src_dir + "/" + filename;
+    if(CompareExtantion(filename, ".png")){
+      dstPath = dst_dir + "/IMG/" + filename;
+    } else {
+      dstPath = dst_dir + "/OTHERS/" + filename;
+    }
+    CopyFile(srcPath, dstPath);      
+  }
+
+}
+
+
+void Work(){
+  //std::cout << "Work\n";
+  while(true){
+    if (is_need_work) {
+      DoWork();
+    }
+    sleep(update_freq);   
+  }
+}
+
+
+
+int main (int argc, char** argv){
+  if (argc != 2) {
+    std::cout << "Bad arguments ( args != 2 )" << std::endl;
+    return -1;
+  }
+
+  int pid = fork();
+  if (pid == -1) {
+    //std::cout << "Error: start daemon failed" << std::endl;
+    exit(EXIT_FAILURE);
+  } else if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  std::string cfgFileName(argv[1]); 
+
+  CreateDaemon(cfgFileName);
+  LoadConfigFile();
+  Init();
+  Work();
+  return 0;
+}
+
