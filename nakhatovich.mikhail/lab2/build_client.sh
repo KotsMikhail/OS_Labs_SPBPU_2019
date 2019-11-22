16e021720b9751b3ba7690a66d49bbe1971f3faf
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-11-22 21:43:58 +0300

    Changed structure of project

--- nakhatovich.mikhail/lab2/build_client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-types_file="types.conf"
-
-for type in $(cat $types_file)
-do
-g++ -std=c++11 -Wall -Werror message.h connect.h conn_$type.cpp user_client.h user_client.cpp client.cpp -o client_$type -pthread -lrt
-done
