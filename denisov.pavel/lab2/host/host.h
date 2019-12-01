8d544a5c0e37150971c0ee4bae054e11a6281f65
Pavel Denisov(PD6) <ppasha9@mail.ru>
2019-12-02 02:45:13 +0300

    DenisovPavel #Lab2 (#27)
    * Add daemon
    
    - "first" final version of daemon
    
    * Fix pid file mistakes
    
    - create pid file in 'build.sh' script
    - do not remove pid file in daemon code
    
    * Fix mistakes
    
    - use 'openlog' function
    - exchange defines WARNING, INFO and ERROR by directly calling 'syslog' function
    - delete unused logging
    
    * Add second lab
    
    * Fix include header
    
    - replace "bits/types/siginfo_t.h" by "csignal"
    
    * Add errno.h
    
    - add errno.h header in conn.h
    
    * Hot fix #1
    
    - remove unused compile parameter from makefile
    - correct log messages in client and host
    
    * Fix issues
    
    - make const variables in host static
    - delete global variables from connections' implementations
    - now you should input new number in host every round
    - add SIGINT singal support in host
    
    * Cosmetic fixes
    
    * Fix issues
    
    - remove 'explicit' in client.h
    - new order of host/client communication
    - new support of additional arguments in connections' implementation
    - add hack for socket connection implementation, in case when client failed and new client is attached
    
    * Hot fix
    
    * Fix waiting client section in host

---
+++ denisov.pavel/lab2/host/host.h
@@ -0,0 +1,63 @@
+#ifndef __HOST_H__
+#define __HOST_H__
+
+#include <conn.h>
+#include <message.h>
+#include <csignal>
+#include <semaphore.h>
+
+class Host
+{
+public:
+    Host (const Host&) = delete;
+    Host & operator=(const Host&) = delete;
+
+    static Host& GetInstance ();
+
+    void  Start            ();
+    bool  IsClientAttached ();
+    void  AttachNewClient  (pid_t clientPid);
+    pid_t GetClientPid     ();
+    void  Terminate        ();
+    void  DeattachClient   ();
+
+    bool OpenConnection    ();
+
+private:
+    struct Client
+    {
+        pid_t pid;
+        bool isAttached;
+
+        int numOfFails;
+
+        Client ()
+        : pid(0)
+        , isAttached(false)
+        , numOfFails(0)
+        {
+        }
+
+        Client (pid_t newPid)
+        : pid(newPid)
+        , isAttached(newPid != 0)
+        , numOfFails(0)
+        {
+        }
+    };
+
+private:
+    Client curClientInfo;
+    Conn   conn;
+    sem_t *semaphore_host, *semaphore_client;
+
+    int curNumber;
+
+    Host ();
+
+    void    ReopenConnection  ();
+    void    ReadNewUserNumber ();
+    Message CountClientStatus (const Message& curClientMessage);
+};
+
+#endif //__HOST_H__
---
+++ denisov.pavel/lab2/host/host_implement.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+#include <csignal>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+
+#include <message.h>
+#include <algorithm>
+
+#include "host.h"
+
+
+static void HostSignalHandler (int signalNum, siginfo_t* info, void* ptr)
+{
+    static Host& host = Host::GetInstance();
+    switch (signalNum) {
+        // synchronize with client signal
+        case SIGUSR1:
+            if (host.IsClientAttached()) {
+                std::cout << "The client has already attached. Is can be only one client." << std::endl;
+            } else {
+                std::cout << "Attaching new client with pid: " << info->si_pid << std::endl;
+                host.AttachNewClient(info->si_pid);
+            }
+            break;
+
+        // client failed signal
+        case SIGUSR2:
+            std::cout << "Client failed." << std::endl;
+            if (host.GetClientPid() == info->si_pid) {
+                host.AttachNewClient(0);
+            }
+            break;
+
+        case SIGTERM:
+        case SIGINT:
+            if (host.IsClientAttached()) {
+                host.DeattachClient();
+            }
+            host.Terminate();
+            break;
+    }
+}
+
+
+Host& Host::GetInstance ()
+{
+    static Host inst;
+    return inst;
+}
+
+
+Host::Host ()
+: curClientInfo(0)
+{
+    struct sigaction sigact;
+    memset(&sigact, 0, sizeof(sigact));
+    sigact.sa_sigaction = HostSignalHandler;
+    sigact.sa_flags = SA_SIGINFO;
+    sigaction(SIGINT, &sigact, NULL);
+    sigaction(SIGTERM, &sigact, NULL);
+    sigaction(SIGUSR2, &sigact, NULL);
+    sigaction(SIGUSR1, &sigact, NULL);
+}
+
+
+bool Host::IsClientAttached ()
+{
+    return curClientInfo.isAttached;
+}
+
+
+void Host::AttachNewClient (pid_t clientPid)
+{
+    curClientInfo = Client(clientPid);
+}
+
+
+void Host::DeattachClient ()
+{
+    kill(GetClientPid(), SIGTERM);
+    AttachNewClient(0);
+}
+
+
+pid_t Host::GetClientPid ()
+{
+    return curClientInfo.pid;
+}
+
+
+bool Host::OpenConnection ()
+{
+    if ((semaphore_host = sem_open(HOST_SEM_NAME, O_CREAT, 0666, 0)) == SEM_FAILED) {
+        std::cout << "[ERROR]: Failed to create host semaphore, error: " << strerror(errno) << std::endl;
+        return false;
+    }
+
+    if ((semaphore_client = sem_open(CLIENT_SEM_NAME, O_CREAT, 0666, 0)) == SEM_FAILED) {
+        std::cout << "[ERROR]: Failed to create client semaphore, error: " << strerror(errno) << std::endl;
+        sem_unlink(HOST_SEM_NAME);
+        return false;
+    }
+
+    std::cout << "Semaphores created. Host's pid: " << getpid() << std::endl;
+    if (!conn.Open(getpid(), true)) {
+        std::cout << "[ERROR]: Failed to open connection." << std::endl;
+        sem_unlink(HOST_SEM_NAME);
+        sem_unlink(CLIENT_SEM_NAME);
+        return false;
+    }
+
+    return true;
+}
+
+
+static bool IsStringNumber (const std::string &str)
+{
+    return !str.empty() &&
+            (std::find_if(str.begin(), str.end(), [](char c){ return !std::isdigit(c); }) == str.end());
+}
+
+
+void Host::ReadNewUserNumber ()
+{
+    std::cout << "Input new host number: " << std::endl;
+    std::string line;
+
+    while (true) {
+        std::getline(std::cin, line);
+
+        if (!IsStringNumber(line)) {
+            std::cout << "Input should be an integer in range [1; 100]. Please, try again..." << std::endl;
+            continue;
+        }
+
+        curNumber = atoi(line.c_str());
+        if (curNumber < 1 || curNumber > 100) {
+            std::cout << "Input number should be in range [1; 100], but received: " << curNumber << ". Please try again..." << std::endl;
+            continue;
+        }
+
+        break;
+    }
+}
+
+
+void Host::ReopenConnection ()
+{
+    sleep(1);
+    conn.Close();
+    conn.Open(getpid(), true);
+}
+
+
+void Host::Start ()
+{
+    struct timespec ts;
+    Message msg;
+    // Job loop
+    while (true) {
+        if (!IsClientAttached()) {
+	    // Waiting while client will be attached - signal SIGUSR1
+            sleep(1);
+            continue;
+        }
+
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_sec += TIMEOUT;
+        sem_post(semaphore_client);
+        if (sem_timedwait(semaphore_host, &ts) == -1) {
+            std::cout << "Client timeouted. Terminating client..." << std::endl;
+            DeattachClient();
+#ifdef host_sock
+            ReopenConnection();
+#endif
+            continue;
+        }
+
+        if (!conn.Read(&msg)) {
+            std::cout << "[ERROR]: Failed to read message from connection interface. Terminating host..." << std::endl;
+            exit(SIGTERM);
+        }
+
+        std::cout << "------------HOST ROUND START-----------" << std::endl;
+        std::cout << "Client's number: " << msg.number << std::endl;
+        ReadNewUserNumber();
+
+        msg = CountClientStatus(msg);
+
+        if (IsClientAttached()) {
+            std::cout << "Client's status: " << ((msg.status == MSG_STATUS::ALIVE) ? "ALIVE" : "DEAD") << std::endl;
+            if (!conn.Write(&msg)) {
+                std::cout << "[ERROR]: Failed to write message in connection interface. Terminating host..." << std::endl;
+                exit(SIGTERM);
+            }
+        }
+        std::cout << "------------HOST ROUND END-------------" << std::endl;
+
+#ifdef host_sock
+        if (!IsClientAttached()) {
+            ReopenConnection();
+        }
+#endif
+    }
+}
+
+
+Message Host::CountClientStatus (const Message& curClientMessage)
+{
+    Message hostMsg(MSG_STATUS::ALIVE, 0);
+
+    if ((curClientMessage.status == MSG_STATUS::ALIVE && abs(curClientMessage.number - curNumber) <= 70) ||
+        (curClientMessage.status == MSG_STATUS::DEAD && abs(curClientMessage.number - curNumber) <= 20)) {
+        curClientInfo.numOfFails = 0;
+    } else {
+        hostMsg.status = MSG_STATUS::DEAD;
+        curClientInfo.numOfFails++;
+        if (curClientInfo.numOfFails == 2) {
+            std::cout << "Client's status: DEAD" << std::endl;
+            std::cout << "Client fails 2 times. Deattaching client..." << std::endl;
+            DeattachClient();
+        }
+    }
+
+    return hostMsg;
+}
+
+
+void Host::Terminate ()
+{
+    std::cout << "Host terminating..." << std::endl;
+    if ((semaphore_host != SEM_FAILED && sem_unlink(HOST_SEM_NAME) == 0) && (semaphore_client != SEM_FAILED && sem_unlink(CLIENT_SEM_NAME) == 0) && conn.Close()) {
+        exit(SIGTERM);
+    }
+    exit(errno);
+}
