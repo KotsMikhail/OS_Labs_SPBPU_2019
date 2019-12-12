f02339ffeab862d9b87a5efdd40fb10fb8dbc69a
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-12-12 13:42:17 +0300

    Added implementation of node

---
+++ nakhatovich.mikhail/lab3/set/node.h
@@ -0,0 +1,29 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <pthread.h>
+
+template<class t>
+class node_t
+{
+public:
+    node_t(const t &item);
+    ~node_t();
+
+    int lock();
+    int unlock();
+
+    t item;
+    node_t<t> *next;
+
+private:
+    node_t() = delete;
+    node_t(node_t const&) = delete;
+    node_t& operator=(node_t const&) = delete;
+
+    pthread_mutex_t _lock;
+};
+
+#include "node.hpp"
+
+#endif // NODE_H
---
+++ nakhatovich.mikhail/lab3/set/node.hpp
@@ -0,0 +1,26 @@
+#include <new>
+
+#include "node.h"
+
+template<class t>
+node_t<t>::node_t(const t &item) : item(item), next(nullptr), _lock(PTHREAD_MUTEX_INITIALIZER)
+{};
+
+template<class t>
+node_t<t>::~node_t()
+{
+    unlock();
+    pthread_mutex_destroy(&_lock);
+}
+
+template<class t>
+int node_t<t>::lock()
+{
+    return pthread_mutex_lock(&_lock);
+}
+
+template<class t>
+int node_t<t>::unlock()
+{
+    return pthread_mutex_unlock(&_lock);
+}
