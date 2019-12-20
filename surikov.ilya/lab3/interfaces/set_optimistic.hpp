0afac0ef326b4cecb29209984196da77a3061aa4
Chopikov <ilyacuri@gmail.com>
2019-12-20 04:55:44 +0300

    Added 3 lab (Surikov Ilya)

---
+++ surikov.ilya/lab3/interfaces/set_optimistic.hpp
@@ -0,0 +1,94 @@
+#include "set_optimistic.h"
+
+template<typename T, typename L>
+SetOptimistic<T, L>::SetOptimistic(): Set<T, L>()
+{}
+
+template<typename T, typename L>
+SetOptimistic<T, L>::~SetOptimistic()
+{
+    for (typename Set<T, L>::Node *to_del : _removed)
+        delete to_del;
+    pthread_mutex_destroy(&_mutex);
+}
+
+template<typename T, typename L>
+bool SetOptimistic<T, L>::add(const T &item)
+{
+    bool success = false;
+    while (true)
+    {
+        typename Set<T, L>::Node *pred = this->_head, *curr = pred->_next;
+        this->loop(pred, curr, item);
+        if (validate(pred, curr))
+        {
+            if (item < curr->_item)
+            {
+                typename Set<T, L>::Node *node = new (std::nothrow) typename Set<T, L>::Node(item);
+                if (node)
+                {
+                    node->_next = curr;
+                    pred->_next = node;
+                    success = true;
+                }
+            }
+            this->unlock(pred, curr);
+            break;
+        }
+        this->unlock(pred, curr);
+    }
+    return success;
+}
+
+template<typename T, typename L>
+bool SetOptimistic<T, L>::remove(const T &item)
+{
+    bool success = false;
+    while (true)
+    {
+        typename Set<T, L>::Node *pred = this->_head, *curr = pred->_next;
+        this->loop(pred, curr, item);
+        if (validate(pred, curr))
+        {
+            if (!(item < curr->_item))
+            {
+                pred->_next = curr->_next;
+                pthread_mutex_lock(&_mutex);
+                _removed.push_back(curr);
+                pthread_mutex_unlock(&_mutex);
+                success = true;
+            }
+            this->unlock(curr, pred);
+            break;
+        }
+        this->unlock(curr, pred);
+    }
+    return success;
+}
+
+template<typename T, typename L>
+bool SetOptimistic<T, L>::contains(const T &item)
+{
+    while (true)
+    {
+        typename Set<T, L>::Node *pred = this->_head, *curr = pred->_next;
+        this->loop(pred, curr, item);
+        if (validate(pred, curr))
+        {
+            this->unlock(pred, curr);
+            return !(item < curr->_item);
+        }
+        this->unlock(pred, curr);
+    }
+}
+
+template<typename T, typename L>
+bool SetOptimistic<T, L>::validate(typename Set<T, L>::Node *pred, typename Set<T, L>::Node *curr)
+{
+    typename Set<T, L>::Node *node = this->_head;
+    while (node->_item < pred->_item) {
+        node = node->_next;
+    }
+    return (node == pred && pred->_next == curr);
+}
+
