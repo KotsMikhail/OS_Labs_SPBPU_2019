2457eb17f8fad184685bd23688ceb422061c288c
bi <dimm98@mail.ru>
2019-12-20 12:57:11 +0300

    Add lab 3 implementation

---
+++ fedorov.dmitrii/lab3/headers/set.h
@@ -0,0 +1,15 @@
+#pragma  once
+#ifndef _SET_H_
+#define _SET_H_
+
+template <typename ELEM_T>
+class Set {
+public:
+   virtual bool Add (ELEM_T elem) = 0;
+   virtual bool Remove (ELEM_T elem) = 0;
+   virtual bool Contains (ELEM_T elem) = 0;
+
+   virtual bool IsEmpty () = 0;
+};
+
+#endif
