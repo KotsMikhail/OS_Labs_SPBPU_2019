796c6c8c377dcdc06b56209a5259083c1079cbbe
ik6cgsg <kozlovilya@bk.ru>
2019-12-22 15:45:54 +0300

    help

---
+++ kozlov.ilya/lab3/src/lazy_list/lazy_list.h
@@ -0,0 +1,43 @@
+#ifndef LAZY_LIST_H
+#define LAZY_LIST_H
+
+#include <set.h>
+#include <collector.h>
+
+template<typename T>
+class LazyList: public Set<T>
+{
+public:
+class LazyNode: public OptimisticList<T>::Node
+  {
+  public:
+    bool marked;
+    LazyNode *next;
+
+    static LazyNode* create(const T& item);
+  private:
+    LazyNode(const T& item, int key, pthread_mutex_t& mutex) noexcept:
+      OptimisticList<T>::Node(item, key, mutex), marked(false), next(nullptr)
+    {
+    }
+  };
+private:
+  LazyNode* head;
+  Collector<LazyNode> collector;
+  const std::string tag = "LazyList";
+
+public:
+  LazyList(LazyNode* head, const Collector<LazyNode>& collector);
+  ~LazyList();
+  bool add(T element) override;
+  bool remove(T element) override;
+  bool contains(T element) const override;
+  bool empty() const override;
+
+private:
+  bool validate(LazyNode* prev, LazyNode* curr) const;
+};
+
+#include "lazy_list.hpp"
+
+#endif // LAZY_LIST_H
